840321ac614062a5bce22a1aacad50a7451771d9
Seo JeongSeop <seo92js@gmail.com>
2023-09-11 16:52:05 +0900

    Initial commit

---
+++ src/main/java/com/seojs/debateking/DebatekingApplication.java
@@ -0,0 +1,13 @@
+package com.seojs.debateking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebatekingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebatekingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seojs/debateking/DebatekingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seojs.debateking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebatekingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
